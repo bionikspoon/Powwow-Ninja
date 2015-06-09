04d4f3712665f6a18c426a2cbe23ae8bcfdb1a4a
Manu <bionikspoon@gmail.com>
2015-06-08 23:33:33 -0500

    main controller

---
+++ client/app/main_old/main.js
@@ -0,0 +1,14 @@
+/*
+'use strict';
+
+angular.module('PowwowNinjaApp')
+
+  .config(function ($stateProvider) {
+    $stateProvider//
+      .state('main', {
+        url: '/main',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
+*/
