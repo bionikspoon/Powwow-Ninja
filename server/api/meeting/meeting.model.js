9f0ae3848130cecaa89359ad93f6eaa3b36b9c50
Manu <bionikspoon@gmail.com>
2015-06-11 18:04:30 -0500

    meetings api

---
+++ server/api/meeting/meeting.model.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MeetingSchema = new Schema({
+  meetingDate: Date,
+  'created_at': {
+    type: Date,
+    default: Date.now
+  },
+  'updated_at': {
+    type: Date,
+    default: Date.now
+  },
+  members: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      rsvp: {
+        date: Date,
+        status: String
+      },
+      checkin: Date,
+      checkout: Date,
+      role: {
+        type: String,
+        default: 'MEMBER'
+      }
+    }
+  ],
+  resources: [
+    {
+      name: String,
+      url: String
+    }
+  ],
+  agenda: [
+    {
+      title: String,
+      status: String,
+      assignments: [
+        {
+          title: String,
+          owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+          },
+          notes: [
+            {
+              author: {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+              },
+              content: String
+            }
+          ],
+          status: String
+        }
+      ]
+    }
+  ],
+  chat: {
+    type: Schema.Types.ObjectId,
+    ref: 'Chat'
+  }
+});
+
+module.exports = mongoose.model('Meeting', MeetingSchema);
--- server/config/seed.js
+++ server/config/seed.js
@@ -7,7 +7,11 @@
 
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
+var Meeting = require('../api/meeting/meeting.model');
+var faker = require('faker');
+var _ = require('lodash');
 
+//noinspection JSUnresolvedFunction
 Thing.find({}).remove(function () {
   Thing.create({
     name: 'Development Tools',
@@ -30,19 +34,138 @@ Thing.find({}).remove(function () {
   });
 });
 
+//noinspection JSUnresolvedFunction
 User.find({}).remove(function () {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function () {
+  var users = [
+    {
+      provider: 'local',
+      name: 'Test User',
+      email: 'test@app.com',
+      password: 'secret'
+    },
+    {
+      provider: 'local',
+      role: 'admin',
+      name: 'Admin',
+      email: 'admin@app.com',
+      password: 'secret'
+    }
+  ];
+
+  _.times(40, function () {
+    users.push({
+      provider: 'local',
+      role: _.sample([
+        'admin',
+        'user'
+      ]),
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: 'secret'
+
+    })
+  });
+
+  User.create(users, function (error) {
+    if (error) { console.error(error); }
+    var users = _.drop(arguments);
     console.log('finished populating users');
+    seedMeeting(users);
   });
 });
+
+function seedMeeting(users) {
+  //noinspection JSUnresolvedFunction
+  Meeting.find({}).remove(function () {
+    var meetings = [];
+    var meeting;
+
+    _.times(20, function () {
+      /**
+       * Scaffold meeting.
+       */
+      meeting = {
+        meetingDate: faker.date.future(1 / 12),
+        members: [],
+        resources: [],
+        agenda: []/*,
+         chat: ''*/
+      };
+
+      /**
+       * Seed meeting members
+       */
+      _.times(_.random(5, 15), function () {
+        //noinspection JSUnresolvedVariable
+        meeting.members.push({
+          user: _.sample(users).id,
+          rsvp: faker.date.recent(), //checkin: Date,
+          //checkout: Date,
+          role: _.sample([
+            'MEMBER',
+            'MODERATOR',
+            'ADMIN'
+          ])
+        });
+      });
+
+      /**
+       * Seed meeting resources
+       */
+      _.times(_.random(3, 10), function () {
+        meeting.resources.push({
+          name: faker.company.bs(),
+          url: faker.image.imageUrl()
+        })
+      });
+
+      /**
+       * Seed meeting agenda
+       */
+      _.times(_.random(5, 15), function () {
+        meeting.agenda.push(populateAgenda(users))
+      });
+
+      meetings.push(meeting);
+    });
+
+    Meeting.create(meetings, function (error) {
+      if (error) { console.error(error); }
+
+      console.log('finished populating meetings');
+    });
+  });
+}
+
+/**
+ * Create an Agenda.
+ * @returns {{title: *, status: string, assignments: Array}}
+ */
+function populateAgenda(users) {
+  var agendaItem = {
+    title: faker.company.catchPhrase(),
+    status: 'open',
+    assignments: []
+  };
+
+  _.times(_.random(5), function () {
+    //noinspection JSUnresolvedVariable
+    var assignment = {
+      title: faker.company.bs(),
+      owner: _.sample(users).id,
+      notes: [
+        {
+          author: _.sample(users).id,
+          content: faker.lorem.sentences()
+        }
+      ],
+      status: _.sample([
+        'OPEN',
+        'CLOSED'
+      ])
+    };
+    agendaItem.assignments.push(assignment);
+  });
+
+  return agendaItem;
+}
